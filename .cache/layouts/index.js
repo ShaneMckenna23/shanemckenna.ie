8e0ad8594bc9ffbf7aa0fcb902ba8ac2e3434bdd
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-01-05 16:49:54 +0000

    Removed build directories

--- .cache/layouts/index.js
+++
@@ -1,7 +0,0 @@
-
-  import React from "react"
-  import Component from "C:/Users/Shane/Documents/GitHub/shanemckenna.ie/src/layouts/index.jsx"
-  import data from "C:\\Users\\Shane\\Documents\\GitHub\\shanemckenna.ie\\.cache\\json\\layout-index.json"
-
-  export default (props) => <Component {...props} {...data} />
-  
