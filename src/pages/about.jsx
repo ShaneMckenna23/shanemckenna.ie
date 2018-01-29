467b736ce85d84858cad853c27e9d07d4241a84b
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-01-29 16:58:58 +0000

    About me updated

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -12,13 +12,19 @@ export default class About extends React.Component {
 				<Helmet title={`About | ${config.siteTitle}`} />
 				<Header>About</Header>
 				<Container text>
-					<h1>Hi!</h1>
+					<h1>Hello!</h1>
 					<p>
-					I am Shane Mckenna, a twenty-one-year-old student from Kerry. 
-					I study Computer Games Development at the University of Limerick. 
-					I have a passion for web development. I find development fun and it 
-					allows me to be creative. I'm constantly learning new and exciting 
-					technologies.
+						My name is Shane Mckenna, I'm a 21-year-old web developer from a small town in Ireland. 
+						I will graduate from college this year. I have a passion for web development, I'm constantly 
+						learning new and exciting technologies.
+					</p>
+					<p>
+						I am very passionate when it comes to music. I love live music and the atmosphere it creates, for this reason, I go 
+						to concerts any chance I get. I am very easy to get along with and I believe I collaborate well with others.
+					</p>
+					<p>
+						Looking for a Full-time web development role with a September start-date. I'm willing to move anywhere for the right 
+						position! I have both EU and USA citizenship.
 					</p>
 				</Container>
 				<Footer />
