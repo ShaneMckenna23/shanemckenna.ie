a58f09a12c1bb8e4ef46040dd0974d1fcae41ac7
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-01-29 16:58:37 +0000

    Contact information

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -12,20 +12,10 @@ export default class Contact extends React.Component {
 				<Helmet title={`Contact | ${config.siteTitle}`} />
 				<Header>Contact</Header>
 				<Container text>
-					<h1>Hi!</h1>
-					<p>
-            Far far away, behind the word mountains, far from the countries
-            Vokalia and Consonantia, there live the blind texts. Separated they
-            live in Bookmarksgrove right at the coast of the Semantics, a large
-            language ocean. A small river named Duden flows by their place and
-            supplies it with the necessary regelialia. It is a paradisematic
-            country, in which roasted parts of sentences fly into your mouth.
-            Even the all-powerful Pointing has no control about the blind texts
-            it is an almost unorthographic life One day however a small line of
-            blind text by the name of Lorem Ipsum decided to leave for the far
-            World of Grammar. The Big Oxmox advised her not to do so, because
-            there were thousands.
-					</p>
+					<h1>E-mail</h1>
+					<p>Shanemckenna23@hotmail.co.uk</p>
+					<h1>Mobile</h1>
+					<p>087-331 4847</p>
 				</Container>
 				<Footer />
 			</div>
