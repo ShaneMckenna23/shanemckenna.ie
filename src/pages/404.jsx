e6b9f31c2dfc6311d404d935c71d5d6ad01a9cc2
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2018-01-29 16:58:20 +0000

    Added new 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import Header from '../components/Header/Header'
+import Container from '../components/Container/Container'
+import Footer from '../components/Footer/Footer'
+import Link from 'gatsby-link'
 
 const NotFoundPage = () => (
-	<div>
-		<h1>NOT FOUND</h1>
-		<p></p>
-	</div>
+	<div className="container contact-container">
+				<Header>404 - Page Note Found</Header>
+				<Container text>
+					<h2>Here are some helpful links instead:</h2>
+						<div><Link to="/">Home</Link></div>
+						<div><Link to="/about">About</Link></div>
+						<div><Link to="/contact/">Contact</Link></div>
+				</Container>
+				<Footer />
+			</div>
 )
 
 export default NotFoundPage
